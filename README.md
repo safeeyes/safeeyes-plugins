4b452c75c3768792c9ea983e7616d0529eecc24e
Gobinath <slgobinath@gmail.com>
2017-10-08 22:02:32 -0400

    Add weather plugin

---
+++ README.md
@@ -0,0 +1,21 @@
+​
+# Safe Eyes Third-party Plugins
+
+This repository is a place for third-party developers to share their plugins written for Safe Eyes. You have the full freedom to distribute and share using any mediums. However, I have created this repository as a place which is easily accessible to the developers via the link shared in the Safe Eyes official website.
+
+## Developers
+I welcome Safe Eyes users to write their own custom plugins and share them here by sending a pull request. However, I do apply some restrictions to the plugins if you want them to be added to this repository:
+
+1. The plugin must be shared under GPL v3 license.
+
+    WHY? I highly believe and support FOSS and among the licenses, I know so far, GPL v3 ensures that the users have full freedom on the code. Further, if anyone copies your code, they must release their entire product under GPL v3 which is good for the FOSS community.
+
+    Please add the GPL v3 license header in `plugin.py` file.
+
+2. The plugin must have an icon.
+
+3. The plugin should be highly cohesive. If you have several ideas, develop them as individual plugins.
+
+## Users
+I try my best to manually analyze the plugins submitted by other developers to ensure that they don't misuse the plugin. I hope other users using Safe Eyes also will keep an eye on this. Anyhow as everything is under GPL v3 license, the plugins are distributed WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
---
+++ weather/plugin.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Safe Eyes is a utility to remind you to take break frequently
+# to protect your eyes from eye strain.
+
+# Copyright (C) 2017  Gobinath
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Show health statistics on the break screen.
+"""
+
+import logging
+import pyowm
+
+api = None
+location = None
+owm = None
+weather_details = None
+
+def init(ctx, safeeyes_config, plugin_config):
+    """
+    Initialize the plugin.
+    """
+    logging.debug('Initialize Weather plugin')
+    global api
+    global location
+    global owm
+    api = plugin_config['api']
+    location = plugin_config['location']
+    if api != "" and location != "":
+        owm = pyowm.OWM(api)
+    
+
+def get_widget_title(break_obj):
+    """
+    Return the widget title.
+    """
+    logging.debug('Get the weather information')
+    global weather_details
+    if owm:
+        try:
+            observation = owm.weather_at_place(location)
+            weather = observation.get_weather()
+            temp = weather.get_temperature('celsius').get('temp')
+            humidity = weather.get_humidity()
+            wind_speed = weather.get_wind().get('speed')
+            weather_details = 'Temperature: %s℃\t\tHumidity: %s\tWind Speed: %s' % (temp, humidity, wind_speed)
+        except BaseException:
+            # Can be a network error
+            weather_details = None
+    if weather_details:
+        return 'Weather'
+
+def get_widget_content(break_obj):
+    """
+    Return the weather details.
+    """
+    return weather_details
